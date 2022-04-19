b7cd9baa743890f5b4c9706638bd1d7a0abd6a15
otienosteve <steveotieno701@gmail.com>
2022-04-19 11:04:51 +0300

    add initial project files

---
+++ main.py
@@ -0,0 +1,65 @@
+# name="Steve"
+#type inferencing
+# num=12.56
+# bool=True
+# print(int(num+12))
+# print(not bool)
+
+#if 
+#for
+
+
+# if bool==True:
+#     print("True")
+#     num=10
+#     if num>5:
+#         print(f"{num} is greater than 5")
+# else:
+#     print("False")
+
+# for( var num=10; num>=1; num--){
+    # num+=1
+# }
+# for p in range(1,10,2):
+#     print(p)
+# var1=1
+# while(var1<=10):
+#     print(var1)
+#     var1+=1
+
+
+ls=[1,2,45,3,4,5,6,7,8,9,10]
+
+# for i in ls:
+#     print(i*2)
+
+#list comprehension
+# ls2=[p*2 for p in ls]
+
+# for i in ls2:
+#     print(i)
+
+# ls3=list(map(lambda x:x**2,ls))
+# print(ls3)
+# tuple
+# tup=(1,2,3,4,5,6,7,8,9,10)
+# print(tup)
+# ls[1]=7
+# tup[1]=7
+# print(ls)
+# ls2=ls.copy()
+
+# ls2[2]=7
+# print(ls)
+# print(ls2)
+# dictionary==object
+
+# dict1={"name":"Steve",
+#         "Occupation":"teacher",
+#         "Age":40}
+# # print(dict1["name"])
+
+# for key,value in dict1.items():
+#     print(key,value)
+
+
